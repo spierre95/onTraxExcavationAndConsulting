df373df9c595c38fb8a4cc45cdd6ae6c865870da
spierre95 <stephen.pierre20@gmail.com>
2019-10-06 17:47:18 -0400

    added contact form, updated styles and page layout

--- public/js/dist/bundle.js
+++ public/js/dist/bundle.js
@@ -3677,17 +3677,31 @@
 })));
 
 },{}],2:[function(require,module,exports){
-var Glide = require('@glidejs/glide')
+var Glide = require("@glidejs/glide");
 // var Images = require('@glidejs/glide/dist/glide.modular.esm');
-var Carousel = new Glide('.glide',{
-    type: 'carousel',
-    perView: 2,
-    gap: 10
-}
-).mount()
+var Carousel = new Glide(".glide", {
+  type: "carousel",
+  perView: 2,
+  gap: 10
+}).mount();
 
-Carousel.on('run.before', evt => {
-    const scrollSteps = Carousel.settings.perView;
-    evt.steps = evt.direction === '>' ? -scrollSteps : scrollSteps;
+// display 2 slides in carousel at one time
+Carousel.on("run.after", evt => {
+  const scrollSteps = Carousel.settings.perView;
+  evt.steps = evt.direction === ">" ? -scrollSteps : scrollSteps;
 });
+
+//smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "start"
+    });
+  });
+});
+
 },{"@glidejs/glide":1}]},{},[2]);
